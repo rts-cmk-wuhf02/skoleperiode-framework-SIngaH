59bd2019fe2f7d98d7f253516aa4a8016a6ecdbf
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-04-21 14:34:03 +0200

    reach router

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
 import regeneratorRuntime from "regenerator-runtime";
+import Details from "./Details";
 
 const App = () => {
   return (
     <React.StrictMode>
       <div>
         <h1 id="something-important">Adopt me!</h1>
-        <SearchParams />
+        <Router>
+          <SearchParams path="/" />
+          <Details path="/details/:id" />
+        </Router>
       </div>
     </React.StrictMode>
   );
