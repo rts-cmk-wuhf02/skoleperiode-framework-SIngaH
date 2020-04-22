e5e75fab0519287e8678f0ad9853aae93be63cfe
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-04-22 08:55:07 +0200

    Carousel Implementation

--- adopt-me/src/Carousel.js
+++ adopt-me/src/Carousel.js
@@ -1,47 +1,47 @@
-import React, { Component } from "react"
+import React, { Component } from "react";
 
-class Carousel extends React, Component{
-    state = {
-        photos:[],
-        active: 0
-    };
-    static getDerivedStateFromProps({ media }){
-        let photos = ["http://placecorgi/600/600"];
+class Carousel extends React.Component {
+  state = {
+    photos: [],
+    active: 0
+  };
+  static getDerivedStateFromProps({ media }) {
+    let photos = ["http://placecorgi/600/600"];
 
-        if(media.length){
-            photos = media.map(({ large}) => large)
-        }
-
-        return { photos };
+    if (media.length) {
+      photos = media.map(({ large }) => large);
     }
 
-    handleIndexClick = event => {
-        this.setState({
-            active: +event.target.dataset.index
-        });
-    };
+    return { photos };
+  }
 
-    render(){
-        const { photos, active } = this.state;
+  handleIndexClick = event => {
+    this.setState({
+      active: +event.target.dataset.index
+    });
+  };
 
-        return(
-            <div className="carousel">
-                <img src={photos[active]} alt="animal"/>
-                <div className="carousel--smaller">
-                    {photos.map((photo, index) => (
-                        <img
-                            key={photo}
-                            onClick={this.handleIndexClick}
-                            data-index={index}
-                            src={photo}
-                            className={index === active ? "active" : ""}
-                            alt="animal thumbnail"
-                        />
-                    ))}
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const { photos, active } = this.state;
+
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            <img
+              key={photo}
+              onClick={this.handleIndexClick}
+              data-index={index}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Carousel
+export default Carousel;
