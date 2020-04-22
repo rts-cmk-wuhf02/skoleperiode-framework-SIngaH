f7666c72b61ed2efc2ec4d566d38e0fabc3bb54d
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-04-22 14:39:37 +0200

    Code Splitting Routes

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,11 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, lazy, Suspense } from "react";
 import { render } from "react-dom";
-import { Router } from "@reach/router";
+import { Router, Link } from "@reach/router";
 import SearchParams from "./SearchParams";
 import regeneratorRuntime from "regenerator-runtime";
-import Details from "./Details";
+
 import ThemeContext from "./ThemeContext";
-import NavBar from "./NavBar";
+
+const Details = lazy(() => import("./Details"));
 
 const App = () => {
   const themeHook = useState("darkblue");
@@ -13,11 +14,15 @@ const App = () => {
     <React.StrictMode>
       <ThemeContext.Provider value={themeHook}>
         <div>
-          <NavBar />
-          <Router>
-            <SearchParams path="/" />
-            <Details path="/details/:id" />
-          </Router>
+          <header>
+            <Link to="/">Adopt me!</Link>
+          </header>
+          <Suspense fallback={<h1>Loading route...</h1>}>
+            <Router>
+              <SearchParams path="/" />
+              <Details path="/details/:id" />
+            </Router>
+          </Suspense>
         </div>
       </ThemeContext.Provider>
     </React.StrictMode>
