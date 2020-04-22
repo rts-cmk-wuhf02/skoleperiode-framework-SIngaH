195a02d1c26626bd12a6885d462c4f86b2cf37a3
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-04-22 14:16:30 +0200

    Template Literals & Hooks

--- adopt-me/src/NavBar.js
+++ adopt-me/src/NavBar.js
@@ -1,21 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "@reach/router";
 import { css } from "@emotion/core";
 
-//to get emoji widows logo + .❤❤
+//to get emoji widows logo + . ❤❤
 
-const NavBar = () => (
-  <header
-    css={css`
-      background-color: #333;
-      padding: 15px;
-    `}
-  >
-    <Link to="/">Adopt me!</Link>
-    <span role="img" aria-label="logo">
-      💕
-    </span>
-  </header>
-);
+const NavBar = () => {
+  const [padding, setPadding] = useState(15);
+  return (
+    <header
+      onClick={() => setPadding(padding + 15)}
+      css={css`
+        background-color: pink;
+        padding: ${padding}px;
+      `}
+    >
+      <Link to="/">Adopt me!</Link>
+      <span
+        css={css`
+          font-size: 60px;
+        `}
+        role="img"
+        aria-label="logo"
+      >
+        💕
+      </span>
+    </header>
+  );
+};
 
 export default NavBar;
