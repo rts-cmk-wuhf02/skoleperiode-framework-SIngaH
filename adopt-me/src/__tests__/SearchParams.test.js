1380dcdb9b1d628f72e924e58151c610d013548a
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-04-23 14:40:01 +0200

    Component Tests issues

---
+++ adopt-me/src/__tests__/SearchParams.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import pet, { ANIMALS, _breeds, _dog } from "@frontendmasters/pet";
+import SearchParams from "../SearchParams";
+
+afterEach(cleanup);
+
+test("SearchParams", async () => {
+  const { getByTestId } = render(<SearchParams />);
+
+  const animalDropDown = getByTestId("use-dropdown-animal");
+  expect(animalDropDown.children.length).toEqual(ANIMALS.length + 1);
+});
