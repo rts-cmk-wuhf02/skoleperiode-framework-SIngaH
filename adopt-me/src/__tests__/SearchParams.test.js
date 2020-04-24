f4d60d8ae866a87eacbf3641530c9dfc9af26f8a
Sigurros Inga Helgadottir <6140228@RTS-365.dk>
2020-04-24 09:54:15 +0200

    need to commit and push before i get the old repo

--- adopt-me/src/__tests__/SearchParams.test.js
+++ adopt-me/src/__tests__/SearchParams.test.js
@@ -6,8 +6,19 @@ import SearchParams from "../SearchParams";
 afterEach(cleanup);
 
 test("SearchParams", async () => {
+  //added after fail container and getByText
   const { getByTestId } = render(<SearchParams />);
+  // const { container, getByTestId, getByText } = render(<SearchParams />);
 
   const animalDropDown = getByTestId("use-dropdown-animal");
   expect(animalDropDown.children.length).toEqual(ANIMALS.length + 1);
+
+  //added after fail
+  // expect(pet.breeds).toHaveBeenCalled();
+  // const breedDropDown = getByTestId("use-dropdown-breeds");
+  // expect(breedDropDown.children.length).toEqual(_breeds.length + 1);
+
+  //added after fail
+  // const searchResults = getByTestId("search-results");
+  // expect(searchResults.textContent).toEqual("No Pets Found")
 });
